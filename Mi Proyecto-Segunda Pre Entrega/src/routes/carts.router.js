d2a3e04381f3c9074a7561045e4a38f9998af6a6
Florencia Palacio <137527381+FlorenciaPalacio@users.noreply.github.com>
2024-10-24 13:47:05 -0300

    Update carts.router.js

--- Mi Proyecto-Segunda Pre Entrega/src/routes/carts.router.js
+++ Mi Proyecto-Segunda Pre Entrega/src/routes/carts.router.js
@@ -3,21 +3,14 @@ const router = express.Router();
 import CartManager from "../managers/cart-manager.js";
 const manager = new CartManager("./src/data/carts.json");
 
-//1) La ruta raíz POST / deberá crear un nuevo carrito
-
 router.post("/", async (req, res) => {
     try {
         const nuevoCarrito = await manager.crearCarrito(); 
         res.json(nuevoCarrito);
     } catch (error) {
-        res.status(500).send("Error teerrribleeee al crear carrito"); 
+        res.status(500).send("Error al crear carrito"); 
     }
 })
-
-//2) La ruta GET /:cid deberá listar los productos que pertenezcan al carrito con el parámetro cid proporcionados.
-//Si hay dudas lo hacemos en clase!
-
-//3) La ruta POST  /:cid/product/:pid deberá agregar el producto al arreglo “products” del carrito seleccionado
 
 router.post("/:cid/product/:pid", async (req, res) => {
     let cartId = req.params.cid;
